66002da0c45fe0467db26d714ff6a91a4611d2f8
Daniel Bogdoll <bogdoll@fzi.de>
2023-05-26 13:52:10 +0200

    GH Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }
+}
 
 module.exports = withContentlayer(nextConfig)
