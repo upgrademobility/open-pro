d922389455af3ccf6ebc7f784d047cffad041080
Daniel Bogdoll <bogdoll@fzi.de>
2023-05-26 14:47:18 +0200

    Reset

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true }
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = withContentlayer(nextConfig)
