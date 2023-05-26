5d199eac6ac1e58ae4eb4a7f423eee416f01a976
Daniel Bogdoll <bogdoll@fzi.de>
2023-05-26 14:19:01 +0200

    try stuff

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  reactStrictMode: true,
-  swcMinify: true
+  images: { unoptimized: true }
 }
 
 module.exports = withContentlayer(nextConfig)
