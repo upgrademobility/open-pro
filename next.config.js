aa74160e45c9e74e4bcac0c2f878c39c748481f5
upgrademobility <132927104+upgrademobility@users.noreply.github.com>
2023-05-26 12:19:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-output: 'export',}
+  output: 'export'
+}
 
 module.exports = withContentlayer(nextConfig)
