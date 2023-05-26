f6dd250b7b158e081ac83560b8a7e28fb54e9fcc
Daniel Bogdoll <bogdoll@fzi.de>
2023-05-26 13:50:10 +0200

    Try vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-}
+const nextConfig = {}
 
 module.exports = withContentlayer(nextConfig)
